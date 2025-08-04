1f2afc9334663197c60bdeb55903114639da2335
nightbwolf <robertovitortroca766@gmail.com>
2025-08-04 16:15:29 -0300

    adicionando função e alerta

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,10 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const nomes = [];
+
+function adicionarAmigo(){
+    const input = document.getElementById('amigo');
+    const nome = input.ariaValueMax.trim()
+
+    if (nome === ""){
+        alert("Por favor, digite um nome válido.");
+    }
+}
